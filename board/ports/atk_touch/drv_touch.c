236dfd4ffa7affc00f0bea922773ed2e32451113
lvhan <lvhan@hnthinker.com>
2023-06-30 17:48:31 +0800

    添加触摸屏驱动

--- board/ports/atk_touch/drv_touch.c
+++ board/ports/atk_touch/drv_touch.c
@@ -14,6 +14,7 @@
 
 #ifdef BSP_USING_TOUCH
 #include "drv_touch.h"
+#include "xpt2046.h"
 
 #define DBG_TAG "drv.touch"
 #define DBG_LVL DBG_LOG
@@ -29,12 +30,50 @@
 extern void lv_port_indev_input(rt_int16_t x, rt_int16_t y, lv_indev_state_t state);
 #endif /* PKG_USING_GUIENGINE */
 
+static struct _rt_drv_touch drv_touch;
+
+__weak int drv_touch_bus_init(struct _rt_drv_touch *config)
+{
+    return RT_EOK;
+}
+
 int drv_touch_hw_init(void)
 {
     rt_err_t result = RT_EOK;
 
+    drv_touch.min_raw_x = 0;
+    drv_touch.min_raw_y = 0;
+    drv_touch.max_raw_x = 4096;
+    drv_touch.max_raw_y = 4096;
+    drv_touch.dev.info.range_x = LCD_WIDTH;
+    drv_touch.dev.info.range_y = LCD_HEIGHT;
+#ifdef TOUCH_USING_XPT2046
+    drv_touch.dev.info.point_num = 1;
+    drv_touch.dev.info.type = RT_TOUCH_TYPE_RESISTANCE;
+    drv_touch.dev.info.vendor = RT_TOUCH_VENDOR_UNKNOWN;
+#endif
+    drv_touch.dev.config.user_data = &drv_touch;
+#ifdef RT_TOUCH_PIN_IRQ
+    drv_touch.dev.config.irq_pin.pin = rt_pin_get(TOUCH_IRQ_PIN);
+    drv_touch.dev.config.irq_pin.mode = PIN_MODE_INPUT_PULLUP;
+#endif /* RT_TOUCH_PIN_IRQ */
+    drv_touch.dev.ops = &xpt2046_ops;
+
+    drv_touch_bus_init(&drv_touch);
+
+    result = rt_hw_touch_register(&drv_touch.dev, "touch", RT_DEVICE_FLAG_INT_RX, &drv_touch);
     return result;
 }
 INIT_DEVICE_EXPORT(drv_touch_hw_init);
+
+int drv_touch_env_init(void)
+{
+#ifdef TOUCH_USING_XPT2046
+    xpt2046_calibration("lcd", "touch");
+#endif
+
+    return RT_EOK;
+}
+INIT_ENV_EXPORT(drv_touch_env_init);
 
 #endif
