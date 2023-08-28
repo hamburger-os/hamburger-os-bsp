ff1393e87731c4912bef77a537de7e71f75c6f28
lvhan <lvhan@hnthinker.com>
2023-08-28 15:37:45 +0800

    添加通过max31826设置系统固化信息

--- packages/sysinfo/sysinfo.h
+++ packages/sysinfo/sysinfo.h
@@ -12,8 +12,8 @@
 
 struct __attribute__ ((packed)) SysInfoDef
 {
-    uint16_t     version;
-    uint8_t     SN[20];
+    uint8_t     version[22];    //系统版本号
+    uint8_t     SN[20];         //核心板编号
     uint32_t    cpu_id[3];
     float       cpu_temp;
     uint8_t     chip_id[8];
@@ -22,16 +22,21 @@ struct __attribute__ ((packed)) SysInfoDef
     uint16_t    count;
 };
 
-struct __attribute__ ((packed)) SysInfoFixDef
+//V0版本系统固化信息
+struct __attribute__ ((packed)) SysInfoFixV0Def
 {
-    uint16_t        version;
-    uint8_t         SN[20];
-    uint8_t         mac[3][6];
-    uint8_t         reserve[84];
-    uint32_t        crc32;
+    uint16_t        version;        //参数版本号:0
+    uint8_t         SN[20];         //核心板编号
+    uint8_t         mac[3][6];      //mac地址
+    uint8_t         reserve[84];    //保留位，需保证数据结构整体为128字节
+    uint32_t        crc32;          //校验值
 };
 
 void sysinfo_get(struct SysInfoDef *info);
 void sysinfo_show(void);
+
+//int8_t sysinfofix_set(void *data);//设置api不提供给应用直接调用
+int8_t sysinfofix_get(void *data);
+void sysinfofix_show(void);
 
 #endif /* PACKAGES_SYSINFO_H_ */
