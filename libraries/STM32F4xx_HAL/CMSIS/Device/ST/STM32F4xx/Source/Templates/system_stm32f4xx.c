0451c6d3b284b13e939edb875a492b33ded4ea22
zm <zhaoming@hnthinker.com>
2023-12-26 10:13:28 +0800

    修改fal_cfg.h包含文件，解决serial_v2.c编译报错问题

--- libraries/STM32F4xx_HAL/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
+++ libraries/STM32F4xx_HAL/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
@@ -48,6 +48,7 @@
 
 #include "stm32f4xx.h"
 #include "board.h"
+#include "fal_cfg.h"
 
 #if !defined  (HSE_VALUE) 
   #define HSE_VALUE    ((uint32_t)25000000) /*!< Default value of the External oscillator in Hz */
